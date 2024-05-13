40f45bf469b2c4783a289bc636b6bd3be199afcb
Max Dumas <max@dumas.nyc>
2024-05-13 16:51:03 -0400

    Fix polygon generation

--- frontend/src/forma-client.ts
+++ frontend/src/forma-client.ts
@@ -1,7 +1,6 @@
 import * as d3 from "d3";
 import { Forma } from "forma-embedded-view-sdk/auto";
 import useSWR from "swr";
-import { FeatureCollection, Polygon } from "geojson";
 import Offset from "polygon-offset";
 import {
   CurveGeometry,
@@ -37,43 +36,12 @@ export function useProjectInfo() {
   );
 }
 
-export async function addMeshElement(
-  geometry: MeshGeometry,
-  properties: { [key: string]: any } = {}
-) {
-  const { urn } = await Forma.integrateElements.createElementHierarchy({
-    authcontext: Forma.getProjectId(),
-    data: {
-      rootElement: "root",
-      elements: {
-        root: {
-          id: "root",
-          properties: {
-            ...properties,
-            geometry: {
-              type: "Inline",
-              format: "Mesh",
-              verts: geometry.vertices,
-              faces: Array(geometry.vertices.length / 3)
-                .fill(0)
-                .map((_, i) => i),
-            },
-            elementProvider: "dynamo-player",
-          },
-        },
-      },
-    },
-  });
-
-  return urn;
-}
-
 export async function addGeojsonElement(
   geometry: CurveGeometry | PointGeometry,
   properties: { [key: string]: any } = {}
 ) {
   const geoJson = createFeatureCollectionWithPolygon(geometry.points);
-  console.log("geoJson", geoJson);
+  console.log("geoJson", JSON.stringify(geoJson));
 
   const { urn } = await Forma.integrateElements.createElementHierarchy({
     authcontext: Forma.getProjectId(),
@@ -89,7 +57,6 @@ export async function addGeojsonElement(
               format: "GeoJSON",
               geoJson,
             },
-            elementProvider: "dynamo-player",
           },
         },
       },
@@ -104,45 +71,26 @@ export type CreateIntegrateElement = {
   geometry: MeshGeometry | PointGeometry | CurveGeometry;
 };
 
-export async function addElement(element: CreateIntegrateElement) {
-  let urn;
-  if (element.geometry.type == "mesh") {
-    urn = await addMeshElement(element.geometry, element.properties);
-  } else if (
-    element.geometry.type == "curve" ||
-    element.geometry.type == "point"
-  ) {
-    urn = await addGeojsonElement(element.geometry, element.properties);
-  } else {
-    throw new Error(`Invalid geometry type`);
-  }
-
-  await Forma.proposal.addElement({ urn });
-}
-
-export const drawPolygon = async (selectedPaths: string[]) => {
+export async function drawPolygon(selectedPaths: string[]) {
   for (let selectPath of selectedPaths) {
     const footPrint = await Forma.geometry.getFootprint({ path: selectPath });
     const coordinates = footPrint?.coordinates;
-    console.log(footPrint);
     let x = -3;
 
     let offset = new Offset();
-    let margined = offset.data(coordinates).margin(10);
-    let padding = offset.data(coordinates).padding(10);
 
     var newCoordinates = offset.data(coordinates).arcSegments(3).offset(x);
-    var polyline = offset.data(coordinates).offsetLine(5);
 
     const polygon: CurveGeometry = {
       type: "curve",
       isClosed: true,
-      points: newCoordinates.reverse(),
+      points: [newCoordinates[0].reverse()],
     };
 
-    addGeojsonElement(polygon);
+    const urn = await addGeojsonElement(polygon);
+    Forma.proposal.addElement({ urn });
   }
-};
+}
 
 export async function compareElements(const_path: string) {
   var selections = await Forma.selection.getSelection();
